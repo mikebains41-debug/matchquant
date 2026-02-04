e6bf6668319e01e502dc21368d3af4240961db08
mikebains41-debug <mikebains41@gmail.com>
2026-02-04 15:57:17 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE = "matchquant-v1";
+
 const ASSETS = [
-  "./",
   "./index.html",
   "./app.js",
   "./xg_tables.json",
@@ -10,21 +10,28 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    )
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).then(net => {
-      const copy = net.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return net;
-    }).catch(() => caches.match("./index.html")))
+    caches.match(e.request).then((res) => {
+      if (res) return res;
+      return fetch(e.request)
+        .then((net) => {
+          const copy = net.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          return net;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
   );
 });
