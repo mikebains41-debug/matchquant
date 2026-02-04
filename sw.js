cc54dd0c94f01b91caed2425ad221d9c23cf387d
mikebains41-debug <mikebains41@gmail.com>
2026-02-04 15:15:20 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
+const CACHE = "matchquant-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./xg_tables.json",
+  "./fixtures.json",
+  "./h2h.json",
+  "./manifest.json"
+];
 
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(net => {
+      const copy = net.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return net;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
